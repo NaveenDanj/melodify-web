63602c4fc1aec4c16fe8cdd4f8a0a041826637ea
naveendanj <naveenhettiwaththa@gmail.com>
2023-07-13 17:40:17 +0530

    added the basic functionality of the melodify music player

--- src/components/global/MusicPlayer.tsx
+++ src/components/global/MusicPlayer.tsx
@@ -8,9 +8,96 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import SlideshowIcon from '@mui/icons-material/Slideshow';
 import QueueMusicIcon from '@mui/icons-material/QueueMusic';
 import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import PauseIcon from '@mui/icons-material/Pause';
+
+
+import { useRef , useState , useEffect } from 'react';
+
 
 
 function MusicPlayer() {
+
+    const audioRef = useRef(null);
+    const volumeRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [totalTime, setTotalTime] = useState(0);
+    const [volume, setVolume] = useState(100);
+
+
+    const handlePlay = () => {
+      audioRef.current.play();
+      setIsPlaying(true);
+    };
+  
+    const handlePause = () => {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    };
+  
+    // const handleVolumeChange = (e) => {
+    //   audioRef.current.volume = e.target.value;
+    // };
+
+    const handleSkipForward = () => {
+        audioRef.current.currentTime += 30;
+    };
+
+    const handleSkipBackward = () => {
+        if (audioRef.current) {
+            audioRef.current.currentTime -= 30;
+        }
+
+    };
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          if (audioRef.current) {
+            setCurrentTime(audioRef.current.currentTime);
+          }
+        }, 100);
+    
+        return () => {
+          clearInterval(interval);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (audioRef.current) {
+          audioRef.current.addEventListener('loadedmetadata', () => {
+            setTotalTime(audioRef.current.duration);
+          });
+        }
+    
+        return () => {
+          if (audioRef.current) {
+            audioRef.current.removeEventListener('loadedmetadata', () => {
+              setTotalTime(audioRef.current.duration);
+            });
+          }
+        };
+    }, []);
+
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+        const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+        return `${minutes}:${seconds}`;
+    };
+
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+        audioRef.current.currentTime = newValue;
+        setCurrentTime( audioRef.current.currentTime);
+    }
+
+
+    const handleVoluemChange = (event: Event, newValue: number | number[]) => {
+        audioRef.current.volume = newValue;
+        setVolume(newValue as number);
+    }
+
+
     return (
         <div className='tw-flex tw-h-full'>
 
@@ -39,21 +126,31 @@ function MusicPlayer() {
 
             
             <div className="tw-flex tw-justify-center tw-flex-grow tw-mx-2">
+
+                <audio ref={audioRef} src="/music/sample.m4a" />
                 
                 <div className="tw-flex tw-flex-col tw-flex-grow tw-max-w-[550px] tw-my-auto">
                     
                     <div className="tw-flex tw-gap-5 tw-justify-center">
                         <ShuffleIcon className="tw-cursor-pointer  tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 25  }} />
-                        <SkipPreviousIcon className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 25  }} />
-                        <PlayCircleFilledIcon className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 35  }} />
-                        <SkipNextIcon className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 25  }} />
+                        <SkipPreviousIcon onClick={handleSkipBackward} className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 25  }} />
+                        
+
+                        {isPlaying ? (
+                            <PauseIcon onClick={handlePause} className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 30  }} />
+                        ) : (
+                            <PlayCircleFilledIcon onClick={handlePlay} className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 30  }} />
+                        )}
+
+
+                        <SkipNextIcon onClick={handleSkipForward} className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 25  }} />
                         <RepeatIcon className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 25  }} />
                     </div>
 
                     <Stack className="tw-mt-2" spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-                        <label className='tw-cursor-pointer tw-text-xs'>2:48</label>
-                        <Slider size="small" style={{ color : '#1DB954' }} value={50} aria-label="Volume" />
-                        <label className='tw-cursor-pointer tw-text-xs'>6:00</label>
+                        <label className='tw-cursor-pointer tw-text-xs'>{formatTime(currentTime)}</label>
+                        <Slider onChange={handleSliderChange}  size="small" style={{ color : '#1DB954' }} max={totalTime} value={currentTime} aria-label="Volume" />
+                        <label className='tw-cursor-pointer tw-text-xs'>{formatTime(totalTime)}</label>
                     </Stack>
 
                 </div>
@@ -63,11 +160,11 @@ function MusicPlayer() {
             </div>
 
             <div className="tw-w-[180px]  tw-my-auto">
-                <div className="tw-flex tw-gap-3">
+                <div className="tw-flex tw-gap-3 tw-mr-5">
                     <SlideshowIcon className="tw-cursor-pointer  tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 20  }} />
                     <QueueMusicIcon className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 20  }} />
                     <VolumeDownIcon className="tw-cursor-pointer tw-my-auto tw0-text-slate-200 hover:tw-text-[#ECECEC]" sx={{ fontSize : 20  }} />
-                    <Slider size="small" style={{ color : '#1DB954' }} className="tw-mr-2" value={50} aria-label="Volume" />                    
+                    <Slider min={0} max={1} step={0.01} defaultValue={1} value={volume} onChange={handleVoluemChange} size="small" style={{ color : '#1DB954' }} className="tw-mr-2" aria-label="Volume" />                    
                 </div>
             </div>
         
