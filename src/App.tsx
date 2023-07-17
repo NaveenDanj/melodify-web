20dd966677896d58d26daa82da821c8bfe399016
naveendanj <naveenhettiwaththa@gmail.com>
2023-07-17 11:40:38 +0530

    added protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppLayout from "src/layouts/AppLayout";
-import Home from "src/pages/App/Home";
+import { BrowserRouter, Routes , Route } from "react-router-dom";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Home from "src/pages/App/Home";
 import Search from "src/pages/App/Search";
 import Playlist from "src/pages/App/Playlist";
 import Queied from "src/pages/App/Queied";
 import ArtistProfile from "src/pages/App/ArtistProfile";
 import UserProfile from "src/pages/App/UserProfile";
+import AppLayout from "src/layouts/AppLayout";
+
+
 import Login from "src/pages/Auth/Login";
 import AuthLayout from "src/layouts/AuthLayout";
 import Register from "src/pages/Auth/Register";
-
+import PrivateRoute from "src/components/routes/ProtectedRoute";
 
 const darkTheme = createTheme({
   palette: {
@@ -29,13 +31,13 @@ function App() {
         <Routes>
 
           <Route path="/" element={<AppLayout />}>
-            <Route index element={<Home />} />
-            <Route path="search" element={ <Search /> } />
-            <Route path="playlist" element={ <Playlist /> } />
-            <Route path="queied" element={ <Queied /> } />
-            <Route path="artist" element={  <ArtistProfile />} />
-            <Route path="user" element={ <UserProfile /> } />
-            {/* <Route path="*" element={<NoPage />} /> */}
+            <Route index element={ <PrivateRoute element={<Home />} /> } />
+            <Route path="search" element={ <PrivateRoute element={<Search />} />  } />
+            <Route path="playlist" element={ <PrivateRoute element={<Playlist />} />  } />
+            <Route path="queied" element={ <PrivateRoute element={<Queied />} />  } />
+            <Route path="artist" element={ <PrivateRoute element={<ArtistProfile />} /> } />
+            <Route path="user" element={ <PrivateRoute element={<UserProfile /> } /> } />
+              {/* <Route path="*" element={<NoPage />} /> */}
           </Route>
 
           <Route path="/auth" element={<AuthLayout />}>
---
+++ src/components/routes/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+
+
+type ProtectedRotueProp = {
+    element : React.ReactNode
+}
+
+const PrivateRoute = ({element}:ProtectedRotueProp) => {
+    
+    const isAuth = false;
+
+    if (isAuth) {
+        return element;
+    } else {
+        return <Navigate to={{ pathname: '/auth/login' }} />;
+    }
+    
+};
+  
+export default PrivateRoute;
